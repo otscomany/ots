435f24ccdba668d52aaff28f011f732a480ffb5c
otsashish <ots.ashish@gmail.com>
2017-09-04 18:05:47 +0530

    new branch

---
+++ my-new-project/app/scripts/controllers/myroute.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myProjectApp.controller:MyrouteCtrl
+ * @description
+ * # MyrouteCtrl
+ * Controller of the myProjectApp
+ */
+angular.module('myProjectApp')
+  .controller('MyrouteCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
